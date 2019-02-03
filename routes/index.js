2a52055e42f80661462cf4b37a62edef5c72fc2f
Duncan Smith <duncanssmith@gmail.com>
2019-02-03 10:33:02 +0000

    Start work on Palm Court upgrade

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('welcome', { title: 'Palm Court Pavilion' });
 });
 
-/* GET consultation page. */
-router.get('/p2_yourOrganisation', function(req, res, next) {
-  res.render('p2_yourOrganisation', { title: 'Senh: Your organisation' });
+/* GET example page. */
+router.get('example', function(req, res, next) {
+  res.render('example', { title: 'Example pug' });
 });
 
-/* GET consultation page. */
-router.get('/p3_about', function(req, res, next) {
-  res.render('p3_about', { title: 'Senh: About' });
+/* GET home page. */
+router.get('menu', function(req, res, next) {
+  res.render('menu', { title: 'Menu' });
 });
 
-/* GET consultation page. */
-router.get('/p4_contact', function(req, res, next) {
-  res.render('p4_contact', { title: 'Senh: Contact' });
-});
-
-/* GET consultation page. */
-router.get('/p5_enquiry', function(req, res, next) {
-  res.render('p5_enquiry', { title: 'Senh: Enquiry' });
-});
 
 module.exports = router;
