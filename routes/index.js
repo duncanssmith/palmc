59a5a3f000444f1a2ef521b08ea49f28320f7f4f
Duncan Smith <duncanssmith@gmail.com>
2019-02-04 12:10:46 +0000

    Update navigation and basic pages

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,32 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/welcome', function(req, res, next) {
-  res.render('welcome', { title: 'Palm Court Pavilion' });
+  res.render('welcome', { title: 'Welcome' });
 });
 
 /* GET example page. */
-router.get('/example', function(req, res, next) {
-  res.render('example', { title: 'Example pug' });
+router.get('/menu', function(req, res, next) {
+  res.render('menu', { title: 'Menu' });
+});
+
+/* GET example page. */
+router.get('/view', function(req, res, next) {
+  res.render('view', { title: 'View' });
+});
+
+/* GET example page. */
+router.get('/book', function(req, res, next) {
+  res.render('book', { title: 'Book' });
+});
+
+/* GET example page. */
+router.get('/functions', function(req, res, next) {
+  res.render('functions', { title: 'Functions' });
+});
+
+/* GET example page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
 });
 
 /* GET home page. */
